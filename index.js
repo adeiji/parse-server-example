c435bcabee9fcbfb77b6f796e10057de0972e406
--replace-all <adeiji@yahoo.com>
2017-05-09 12:12:28 -0700

    Added mailgun

--- index.js
+++ index.js
@@ -19,7 +19,26 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+    fromAddress: process.env.EMAIL_FROM || "	sandbox78ae0ed0f5b34d43bedffc6e30b1ac62.mailgun.org",
+    domain: process.env.MAILGUN_DOMAIN || "mailgun.org",
+    apiKey: process.env.MAILGUN_API_KEY || "key-0cd8ecc82f2cd6bc062287ced4ad5a98",
+    // Verification email subject
+    verificationSubject: 'Please verify your e-mail for %appname%',
+    // Verification email body
+    verificationBody: 'Hi,\n\nYou are being asked to confirm the e-mail address %email% with %appname%\n\nClick here to confirm it:\n%link%',
+
+    // Password reset email subject
+    passwordResetSubject: 'Password Reset Request for %appname%',
+    // Password reset email body
+    passwordResetBody: 'Hi,\n\nYou requested a password reset for %appname%.\n\nClick here to reset it:\n%link%',
+    //OPTIONAL (will send HTML version of email):
+    passwordResetBodyHTML: "<!--DOCTYPE html>........"
+    }
+ }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -53,3 +72,6 @@ httpServer.listen(port, function() {
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
+
+
+
